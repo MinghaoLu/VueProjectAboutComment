cfea556972af124fa26ca16989a9797e7d63674b
MinghaoLu <568432983@qq.com>
2018-07-07 16:46:56 +0800

    一条评论

---
+++ src/components/SingleComment.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="singleComment">
+        <p>{{comment.content}}</p>
+        <span class="publisher">{{comment.publisher}}</span>
+        <span class="createAt">{{comment.created_at}}</span>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        data: function() {
+            return {
+
+            }
+        },
+        props: ['comment'],
+    }
+</script>
+
+<style type="text/css">
+    .singleComment {
+        width: 40%;
+        border: 1px dotted black;
+        margin-left: 30%;
+        margin-top: 10px;
+    }
+</style>
